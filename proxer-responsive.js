9dfb663d535923d02dbf67b81d4da82d97b36686
Xylian <33781686+LuceusXylian@users.noreply.github.com>
2022-01-15 00:32:50 +0100

    Initial commit

---
+++ proxer-responsive.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         proxer-responsive.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Force proxer.me to be responsive
+// @author       Xylian
+// @match        https://proxer.me/*
+// @grant 		 GM_setValue
+// @grant 		 GM_getValue
+// ==/UserScript==
+
+(function() {
+	// inject importent meta tags for responsive design
+	document.head.insertAdjacentHTML('beforeend', `<meta http-equiv="X-UA-Compatible" content="IE=edge">`);
+	document.head.insertAdjacentHTML('beforeend', `<meta name="viewport" content="width=device-width, initial-scale=1">`);
+
+	// inject Stylesheet
+	document.head.insertAdjacentHTML('beforeend', 
+		`<style>
+			#nav, #innerNav, #outerNav, #panel { position: unset !important; min-width: 300px; max-width: 100%; height: auto; clear: both; }
+			#rightNav { display: block; width: 100%; }
+			#nav li.borderLeft { border-left: 0; }
+			#wrapper { min-width: 300px; max-width: 100%; background: unset; }
+			#top { position: unset; margin: 0; }
+			#main { min-width: 300px; margin: 0; border: 0; border-radius: 0; }
+			#headSection { display: none; }
+			.inner { min-width: 300px; width: 100%; max-width: 100%; margin: 0; padding: 0 }
+			table, tbody, theader, tfooter, tr { display: block; width: 100%; }
+			td { display: block; width: 100%; }
+			.proxerSlider { display: none !important; }
+			#simple-navi { border-radius: 0; }
+			#simple-navi, #simple-navi-wiki, #simple-navi li, ul#simple-navi-wiki li { position: unset; float: unset; }
+			ul#simple-navi li, ul#simple-navi-wiki li { float: unset; display: inline-block; margin: 2px; }
+			ul#simple-navi li a, ul#simple-navi-wiki li a { border-radius: 10px; }
+
+			#box-table-a tr { height: auto; }
+			#box-table-a td { display: inline-block; border-bottom: 0; }
+
+			table.details td { display: table-cell; min-width: 50%; width: 1000px; max-width: 100%; }
+			table.details td div { width: 100% !important; }
+
+			#panel #navigation { display: none; }
+			#readers { position: unset !important; }
+			#readers .menu { width: 40px; min-height: 40px; }
+			#readers > a { width: 25px; padding: 0; text-align: center; }
+			#reader img { pointer-events: none; cursor: default; max-width: 100%; height: auto;}
+		</style>`
+	);
+
+	// Manga Reader Stuff
+	var reader = document.getElementById("reader");
+	if (reader !== null) {
+		var nextChapterButton = reader.appendChild(
+			document.createElement("a")
+		);
+		nextChapterButton.id = "nextChapterButton";
+		nextChapterButton.className = "menu";
+		nextChapterButton.innerText = "Nächstes Kapitel";
+		nextChapterButton.style.width = "80%";
+		nextChapterButton.style.margin = "10px 0";
+
+		nextChapterButton.addEventListener("click", function() {
+			document.getElementById("chapterImage0").click();
+		});
+		
+	}
+})();
