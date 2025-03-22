ba4dbc5ba9e5b7ef3884b03e31c93a1dd958f6aa
Xylian <33781686+LuceusXylian@users.noreply.github.com>
2025-03-22 20:35:55 +0100

    Update proxer-responsive.js

--- proxer-responsive.js
+++ proxer-responsive.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name         proxer-responsive.js
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
+// @updateURL    https://raw.githubusercontent.com/LuceusXylian/proxer-responsive/main/proxer-responsive.js
+// @downloadURL  https://raw.githubusercontent.com/LuceusXylian/proxer-responsive/main/proxer-responsive.js
 // @description  Force proxer.me to be responsive
 // @author       Xylian
 // @match        https://proxer.me/*
@@ -15,14 +17,19 @@
 	document.head.insertAdjacentHTML('beforeend', `<meta name="viewport" content="width=device-width, initial-scale=1">`);
 
 	// inject Stylesheet
-	document.head.insertAdjacentHTML('beforeend', 
+	document.head.insertAdjacentHTML('beforeend',
 		`<style>
-			#nav, #innerNav, #outerNav, #panel { position: unset !important; min-width: 300px; max-width: 100%; height: auto; clear: both; }
+			* { max-width: 100%; }
+            html { width: 100vw; overflow: hidden auto; }
+
+            #nav, #innerNav, #outerNav, #panel { position: unset !important; min-width: 300px; max-width: 100%; height: auto; clear: both; }
 			#rightNav { display: block; width: 100%; }
 			#nav li.borderLeft { border-left: 0; }
 			#wrapper { min-width: 300px; max-width: 100%; background: unset; }
 			#top { position: unset; margin: 0; }
 			#main { min-width: 300px; margin: 0; border: 0; border-radius: 0; }
+            #loginBubble { z-index: 100; }
+
 			#headSection { display: none; }
 			.inner { min-width: 300px; width: 100%; max-width: 100%; margin: 0; padding: 0 }
 			table, tbody, theader, tfooter, tr { display: block; width: 100%; }
@@ -33,8 +40,11 @@
 			ul#simple-navi li, ul#simple-navi-wiki li { float: unset; display: inline-block; margin: 2px; }
 			ul#simple-navi li a, ul#simple-navi-wiki li a { border-radius: 10px; }
 
-			#box-table-a tr { height: auto; }
-			#box-table-a td { display: inline-block; border-bottom: 0; }
+			#box-table-a tr { height: auto !important; }
+            #box-table-a tr, #box-table-a td { background: #000; }
+            #box-table-a tr:first-child, #box-table-a th { background: #8A0E0E; }
+            #box-table-a tr:not(:first-child):hover { background: #FFF !important; color: #000 !important; }
+			#box-table-a th, #box-table-a td { display: inline-block; border-bottom: 0; width: calc(25% - 20px); }
 
 			table.details td { display: table-cell; min-width: 50%; width: 1000px; max-width: 100%; }
 			table.details td div { width: 100% !important; }
@@ -62,6 +72,6 @@
 		nextChapterButton.addEventListener("click", function() {
 			document.getElementById("chapterImage0").click();
 		});
-		
+
 	}
 })();
